763311479e54c453ac729b383f9203522dba90d3
HellCAT0147 <letscheckmail23@gmail.com>
2023-08-15 19:07:56 +0500

    feat: update gitignore to include config file

--- src/components/e-commerce-api-config.ts
+++ src/components/e-commerce-api-config.ts
@@ -1,1 +1,20 @@
-class ECommerceAPIConfig {}
+export class ECommerceAPIConfig {
+  public projectKey: string;
+
+  public clientId: string;
+
+  public clientSecret: string;
+
+  public region: string;
+
+  constructor() {
+    this.projectKey = 'TEMPLATE';
+    this.clientId = 'TEMPLATE';
+    this.clientSecret = 'TEMPLATE';
+    this.region = 'TEMPLATE';
+  }
+}
+
+const eCommerceAPIConfig: ECommerceAPIConfig = new ECommerceAPIConfig();
+
+export default eCommerceAPIConfig;
