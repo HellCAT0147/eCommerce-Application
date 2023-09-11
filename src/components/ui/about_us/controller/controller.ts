16e464e3f3bd339c9058b5852ca05850ff965d9c
a-reznikov <a.reznnikov@gmail.com>
2023-09-11 15:55:38 +0300

    feat: add style to header and burger menu

--- src/components/ui/about_us/controller/controller.ts
+++ src/components/ui/about_us/controller/controller.ts
@@ -1,5 +1,6 @@
 class ControllerAboutUS {
   public mouseEvent(e: MouseEvent): void {
+    e.preventDefault();
     const { target } = e;
     // TODO implement delegating mouse event for about us pages
   }
