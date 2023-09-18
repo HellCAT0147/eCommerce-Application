8e78c4ccefe65f2e446cc11332b590168b672ae5
a-reznikov <a.reznnikov@gmail.com>
2023-09-18 16:59:57 +0300

    feat: add team member info (Aleksandr)

--- src/components/models/person.ts
+++ src/components/models/person.ts
@@ -19,7 +19,7 @@ export enum Person2 {
   city = 'Omsk',
   country = 'Russia',
   impact = 'Kamilla was mainly dealing with API interaction and catalog page. Besides, she spent much time in Commercetools website, filling in product info and pictures and setting up project settings.',
-  bio = "Kamilla started her way to IT from Stage 0 in 2023. Before it she used to be a teacher, so changing profession too a lot of effort. Married to a software engineer, owns a horse and 2 ginger cats. She likes Vanilla js, cross-stitching and 17'  laptops. In her free time she usually goes to the forest to cry out loud over ESLint function length limitation. Strongly prefers using switch(true) instead of chain of if-elses. Windows user (yet...).",
+  bio = "Kamilla started her way to IT from Stage 0 in 2022. Before it she used to be a teacher, so changing profession too a lot of effort. Married to a software engineer, owns a horse and 2 ginger cats. She likes Vanilla js, cross-stitching and 17'  laptops. In her free time she usually goes to the forest to cry out loud over ESLint function length limitation. Strongly prefers using switch(true) instead of chain of if-elses. Windows user (yet...).",
 }
 
 export enum Person3 {
@@ -30,8 +30,8 @@ export enum Person3 {
   email = 'a.reznnikov@gmail.com',
   city = 'Gomel',
   country = 'Belarus',
-  impact = 'Lorem ipsum dolor sit amet. Aut exercitationem amet est dolor eaque At nemo quibusdam. Nam laborum libero a aperiam voluptas ut iusto atque et perspiciatis porro a magni distinctio ut quos voluptatem.',
-  bio = 'Lorem ipsum dolor sit amet. Qui minus numquam et aliquam quasi qui explicabo voluptas eum amet laborum in ipsa voluptatem. In voluptatem dolorem id beatae aliquam ab rerum error aut dolores assumenda et ratione voluptate. Rem odio sunt sed deserunt iste ut corrupti quasi et laborum dolorem. Et repellendus odit qui nobis laboriosam rem quisquam veniam aut error odio? Eum corporis cupiditate et illum deserunt et inventore alias ut eius corrupti eum minus ullam est distinctio galisum aut dolor dolorem.',
+  impact = 'Alexander developed the routing on pure TS and created a MVC for each page of the project. He worked with the design and developed Builder class that generates HTML elements with classes according to BEM methodology. He customized the project structure, configs, scripts and env.',
+  bio = 'Just like his colleagues, Alexander started learning JS in December last year on a basic course from RS school. Within the first two weeks he realized that frontend was what he was looking for. He dedicated about 70% of his time to learning, which allowed him to rank at the top. His work Minesweeper made it to the list of top 10 best jobs. One of the toughest assignments was Clean Code, but it was after that - he fell in love with clean code and continues to improve this skill. He loves development and wants it to be his job.',
 }
 
 export enum Mentor1 {
